2cca2f727b5888dda27ef2cba5fc3972967fc926
ycchen <ycchen@doohui.com>
2017-05-25 17:38:45 +0800

    准备上线

--- TestClient.py
+++ TestClient.py
@@ -7,9 +7,11 @@ EASecure = EASecure()
 testKey = '29dQrqxAJOgHA3IC5kXYNscvfjAOEB7u'
 testData = b'\x02\x00\x00\x00\x07\x010\xe7\x03\x00\x00\x03|d14c4814e3834bc508ba8be48b1ea99a|999201705265.13:54:24.98541'
 
+print(len(testData))
+
 sk = socket.socket()
-sk.connect(("120.25.97.61", 9999))  # 主动初始化与服务器端的连接
-sk.send(b'\x02\x00\x00\x00\x07\x010\xe7\x03\x00\x00\x03')
+sk.connect(("127.0.0.1", 9999))  # 主动初始化与服务器端的连接
+sk.send(b'\x02\x00\x00\x00\x07\x010\xe7\x03\x00\x00\x03|d14c4814e3834bc508ba8be48b1ea99a|999201705265.13:54:24.98541')
 accept_data = sk.recv(1024)
 print(accept_data)
 sk.close()
